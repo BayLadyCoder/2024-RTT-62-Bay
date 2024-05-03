25ea2b0f592c1dc35e8059f9fc19adaae0f0f9e8
BayLadyCoder <rachada_32@hotmail.com>
2024-05-02 21:09:31 -0400

    PA 303.2.3

---
+++ java-classwork/Java-Classwork/src/main/java/org/example/pa_303_2_3/Task6.java
@@ -0,0 +1,21 @@
+package org.example.pa_303_2_3;
+
+public class Task6 {
+    public static void main(String[] args) {
+        // Write a program that demonstrates at least three ways
+        // to increment a variable by 1 and does this multiple times.
+        // Assign a value to an integer variable, print it,
+        // increment by 1, print it again, increment by 1, and then print it again.
+
+        int num = 0;
+        System.out.println(num);
+        num = num + 1;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num += 1;
+        System.out.println(num);
+        ++num;
+        System.out.println(num);
+    }
+}
