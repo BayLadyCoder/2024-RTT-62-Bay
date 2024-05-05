e9f8352b47946bc96eae039f46f9079977f489f5
BayLadyCoder <rachada_32@hotmail.com>
2024-05-05 18:02:26 -0400

    PA 303.4.1 Task 1

---
+++ java-classwork/Java-Classwork/src/main/java/org/example/pa_303_4_1/Task1.java
@@ -0,0 +1,12 @@
+package org.example.pa_303_4_1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int x;
+        x = 15;
+
+        if(x < 10) {
+            System.out.println("Less than 10");
+        }
+    }
+}
