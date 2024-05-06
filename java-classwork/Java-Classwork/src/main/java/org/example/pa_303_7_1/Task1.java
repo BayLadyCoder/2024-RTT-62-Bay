4f55a2cfafc2fae28d0bb5993dd9cf55b7672da7
BayLadyCoder <rachada_32@hotmail.com>
2024-05-06 17:30:15 -0400

    PA 303.7.1 Task 1

---
+++ java-classwork/Java-Classwork/src/main/java/org/example/pa_303_7_1/Task1.java
@@ -0,0 +1,16 @@
+package org.example.pa_303_7_1;
+
+import java.lang.reflect.Array;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        for(int i=0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
