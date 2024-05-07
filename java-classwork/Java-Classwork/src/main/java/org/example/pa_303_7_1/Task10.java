a8682980fd3125d2b93ea93507fd8f188d682496
BayLadyCoder <rachada_32@hotmail.com>
2024-05-06 21:13:50 -0400

    PA 303.7.1 Task 10

---
+++ java-classwork/Java-Classwork/src/main/java/org/example/pa_303_7_1/Task10.java
@@ -0,0 +1,10 @@
+package org.example.pa_303_7_1;
+
+import java.util.Arrays;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Object[] arr = {"cat", 5, "dog", "bird", 10.19};
+        System.out.println(Arrays.toString(arr));
+    }
+}
