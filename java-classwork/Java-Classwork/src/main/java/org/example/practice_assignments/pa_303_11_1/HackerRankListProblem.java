256a75631140baffbac1d47e650c06c8169475e8
BayLadyCoder <rachada_32@hotmail.com>
2024-05-15 23:48:19 -0400

    PA 303.11.1 HackerRank List

---
+++ java-classwork/Java-Classwork/src/main/java/org/example/practice_assignments/pa_303_11_1/HackerRankListProblem.java
@@ -0,0 +1,46 @@
+package org.example.practice_assignments.pa_303_11_1;
+
+import java.util.*;
+
+// https://www.hackerrank.com/challenges/java-list/problem
+public class HackerRankListProblem {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        List<Integer> nums = new ArrayList<Integer>(length);
+
+        for(int i=0; i < length; i++){
+            Integer num = scanner.nextInt();
+            nums.add(num);
+        }
+
+        int queries = scanner.nextInt();
+
+        for(int i=0; i < queries; i++){
+            scanner.nextLine();
+            String command = scanner.nextLine();
+            if(command.equals("Insert")) {
+                int indexToInsertAt = scanner.nextInt();
+                Integer num = scanner.nextInt();
+
+                if(indexToInsertAt < nums.size()){
+                    nums.add(indexToInsertAt, num);
+                } else {
+                    nums.add(num);
+                }
+
+            } else if(command.equals("Delete")){
+                int indexToRemove = scanner.nextInt();
+                nums.remove(indexToRemove);
+
+            }
+        }
+
+        for(Integer num: nums) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}
