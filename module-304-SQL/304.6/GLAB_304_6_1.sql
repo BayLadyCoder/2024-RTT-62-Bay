0ecdeaeda65d5ab9d45bb60c8e2045db31855ae5
BayLadyCoder <rachada_32@hotmail.com>
2024-05-31 18:40:53 -0400

    GLAB 304.6.1 problem 2

--- module-304-SQL/304.6/GLAB_304_6_1.sql
+++ module-304-SQL/304.6/GLAB_304_6_1.sql
@@ -11,3 +11,17 @@ JOIN employees e
 ON c.sales_rep_employee_id = e.id
 ORDER BY c.customer_name;
 
+-- 2. To determine which products are the most popular with our customers. 
+-- For each product, list the total quantity ordered, 
+-- along with the total sale generated (total quantity ordered * priceEach) for that product. 
+-- The column headers should be “Product Name,” “Total # Ordered,” 
+-- and “Total Sale.” List the products by “Total Sale” descending.
+SELECT 
+	p.product_name AS "Product Name",
+	SUM(od.quantity_ordered) AS "Total # Ordered", 
+    SUM(od.quantity_ordered * od.price_each) AS "Total Sale"
+FROM products p, orderdetails od
+WHERE od.product_id = p.id
+GROUP BY p.id
+ORDER BY "Total Sale" DESC;
+
