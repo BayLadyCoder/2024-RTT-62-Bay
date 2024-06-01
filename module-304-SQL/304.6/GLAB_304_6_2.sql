089f0b9165d0ee0ff79b899ca11954bbf7525187
BayLadyCoder <rachada_32@hotmail.com>
2024-05-31 20:01:21 -0400

    GLAB 304.6.2 problem 2

--- module-304-SQL/304.6/GLAB_304_6_2.sql
+++ module-304-SQL/304.6/GLAB_304_6_2.sql
@@ -4,3 +4,10 @@ FROM product p
 JOIN product_type pt
 ON p.product_type_cd = pt.product_type_cd;
 
+-- 2. For each branch, list the branch name and city, 
+-- plus the last name and title of each employee who works in that branch.
+SELECT b.name, b.city, e.last_name, e.title
+FROM branch b
+JOIN employee e
+ON e.assigned_branch_id = b.branch_id;
+
