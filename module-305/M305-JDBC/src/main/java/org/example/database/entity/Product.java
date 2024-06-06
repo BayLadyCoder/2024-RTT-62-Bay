6e1825fd0e5bf5319e31034896026d6e8edd63e3
BayLadyCoder <rachada_32@hotmail.com>
2024-06-05 22:41:00 -0400

    Update Product entity columns definitions and add more Lombok annotations

--- module-305/M305-JDBC/src/main/java/org/example/database/entity/Product.java
+++ module-305/M305-JDBC/src/main/java/org/example/database/entity/Product.java
@@ -1,12 +1,14 @@
 package org.example.database.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Table (name = "products")
 public class Product {
     @Id // this is telling hibernate this column is the PK
@@ -14,30 +16,30 @@ public class Product {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "product_code")
+    @Column(name = "product_code", length = 15)
     private String productCode;
 
-    @Column(name = "product_name")
+    @Column(name = "product_name", length = 70)
     private String productName;
 
     @Column(name = "productline_id")
     private Integer productlineId;
 
-    @Column(name = "product_scale")
+    @Column(name = "product_scale", length = 10)
     private String productScale;
 
-    @Column(name = "product_vendor")
+    @Column(name = "product_vendor", length = 50)
     private String productVendor;
 
-    @Column(name = "product_description", length = 65535, columnDefinition = "Text")
+    @Column(name = "product_description", length = 65535, columnDefinition = "text")
     private String productDescription;
 
-    @Column(name = "quantity_in_stock")
+    @Column(name = "quantity_in_stock", columnDefinition = "smallint")
     private Integer quantityInStock;
 
-    @Column(name = "buy_price")
+    @Column(name = "buy_price", columnDefinition = "decimal(10,2)")
     private Float buyPrice;
 
-    @Column(name = "msrp")
+    @Column(name = "msrp", columnDefinition = "decimal(10,2)")
     private Float msrp;
 }
