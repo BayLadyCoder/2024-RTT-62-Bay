e7e0cc3cbd4d26ad97f6778fd63d14b6636adcc1
BayLadyCoder <rachada_32@hotmail.com>
2024-06-07 22:04:34 -0400

    Add relationships in OrderDetail entity

--- module-305/M305-JDBC/src/main/java/org/example/database/entity/OrderDetail.java
+++ module-305/M305-JDBC/src/main/java/org/example/database/entity/OrderDetail.java
@@ -16,10 +16,20 @@ public class OrderDetail {
     @Column(name = "id")
     private Integer id;
 
-    @Column(name = "order_id")
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "order_id", insertable=false, updatable=false)
     private Integer orderId;
 
-    @Column(name = "product_id")
+    @Column(name = "product_id", insertable=false, updatable=false)
     private Integer productId;
 
     @Column(name = "quantity_ordered")
