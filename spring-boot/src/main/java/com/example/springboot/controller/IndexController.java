b9f6584e8b404916ffc50f4ae8261742ffc4cb0a
BayLadyCoder <rachada_32@hotmail.com>
2024-07-09 11:28:56 -0400

    Use JSP to display dynamic HTML in Spring Boot application

---
+++ spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView response = new ModelAndView("index");
+        return response;
+    }
+
+    @GetMapping("/another-page-url")
+    public ModelAndView anotherPage() {
+        ModelAndView response = new ModelAndView("another-page");
+        return response;
+    }
+}
