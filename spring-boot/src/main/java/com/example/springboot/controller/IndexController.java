137d8c6f1bfad6398df7a7c42aafeae4cc14dc9a
BayLadyCoder <rachada_32@hotmail.com>
2024-07-11 23:01:21 -0400

    Use @RequestParam and @PathVariable in Controller

--- spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
+++ spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
@@ -1,15 +1,40 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.database.dao.ProductDAO;
+import com.example.springboot.database.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private ProductDAO productDAO;
+
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(@RequestParam(required = false) Integer id) { // http://localhost:8080/?id=2
         ModelAndView response = new ModelAndView("index");
+        Product product = productDAO.findById(id);
         response.addObject("message", "Hello World!");
+        response.addObject("product", product);
+
+        return response;
+    }
+
+    @GetMapping("/index/{id}")
+//    public ModelAndView indexPathVar(@PathVariable Integer id) { // http://localhost:8080/index/1
+    public ModelAndView indexPathVar(@PathVariable(name = "id") Integer productId) { // http://localhost:8080/index/1
+        ModelAndView response = new ModelAndView("index");
+        Product product = productDAO.findById(productId);
+        response.addObject("message", "Hello World!");
+        response.addObject("product", product);
+
         return response;
     }
 
@@ -18,4 +43,5 @@ public class IndexController {
         ModelAndView response = new ModelAndView("another-page");
         return response;
     }
+
 }
--- spring-boot/src/main/webapp/WEB-INF/jsp/index.jsp
+++ spring-boot/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -3,4 +3,11 @@
     <h1>This is my first JSP page!</h1>
     <h2>${message}</h2>
 
+    <table>
+        <tr><td>id</td><td>${product.id}</td></tr>
+        <tr><td>code</td><td>${product.productCode}</td></tr>
+        <tr><td>name</td><td>${product.productName}</td></tr>
+        <tr><td>description</td><td>${product.productDescription}</td></tr>
+    </table>
+
 <jsp:include page="include/footer.jsp" />
