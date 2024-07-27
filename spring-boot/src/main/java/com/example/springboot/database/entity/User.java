d7fb895d4ed323ef9ac304bc627dda4c6c8504f5
BayLadyCoder <rachada_32@hotmail.com>
2024-07-26 21:32:58 -0400

    User and UserRole entities

---
+++ spring-boot/src/main/java/com/example/springboot/database/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.springboot.database.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "new_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "create_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+}
