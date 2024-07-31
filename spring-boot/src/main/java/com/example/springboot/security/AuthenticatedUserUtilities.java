336e110b088240f9deea5fc12cc2649ccd02ced9
BayLadyCoder <rachada_32@hotmail.com>
2024-07-31 11:25:09 -0400

    Add isUserInRole() and isAuthenticated() to AuthenticatedUserUtilities

--- spring-boot/src/main/java/com/example/springboot/security/AuthenticatedUserUtilities.java
+++ spring-boot/src/main/java/com/example/springboot/security/AuthenticatedUserUtilities.java
@@ -10,10 +10,13 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Slf4j
 @Component
@@ -56,7 +59,30 @@ public class AuthenticatedUserUtilities {
         sc.setAuthentication(result);
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);
     }
-    
+
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    public boolean isUserInRole(String role) {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if (context != null && context.getAuthentication() != null) {
+            Collection<? extends GrantedAuthority> authorities = context.getAuthentication().getAuthorities();
+            for (GrantedAuthority authority : authorities) {
+                if (authority.getAuthority().equals(role)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return false;
+        }
+
+        return (authentication != null && authentication.isAuthenticated());
+    }
 }
